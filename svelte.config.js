75ff97dbcfa60bda0b07499ed517a8fb84f5b36f
Michael Cahana <cahanamichael@gmail.com>
2025-05-27 13:46:42 -0400

    try changing configs

--- svelte.config.js
+++ svelte.config.js
@@ -12,14 +12,10 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter({
-      pages: "build",
-      assets: "build",
-      fallback: "index.html",
-      precompress: false,
-      strict: true,
+      fallback: "404.html",
     }),
     paths: {
-      base: process.argv.includes("dev") ? "" : "/nyc_towing",
+      base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
     },
   },
 };
